6f830dbb948c590b5928215ceb9f49f88c0f2c68
island-ZRY <59489854@qq.com>
2021-07-29 11:25:21 +0800

    update 2021/07/29 11:24

---
+++ Python programming from entry to practice/Chapter 6/pkg_2/ry5.py
@@ -0,0 +1,16 @@
+alien_0={'x_position':0,'y_position':25,'speed':'medium'}
+print(f"Original position: {alien_0['x_position']}")
+
+# 向右移动外星人
+# 根据当前速度确定将外星人向右移动多远
+
+if alien_0['speed']=='slow':
+    x_increment=1
+elif alien_0['speed']=='medium':
+    x_increment=2
+else:
+    x_increment=3
+
+alien_0['x_position']=alien_0['x_position']+x_increment
+
+print(f"New position: {alien_0['x_position']}")
---
+++ Python programming from entry to practice/Chapter 6/pkg_3/ry6.py
@@ -0,0 +1,9 @@
+favorite_languages={
+    'jen':'python',
+    'sarah':'c',
+    'edward':'ruby',
+    'phil':'python',
+}
+
+for name in sorted(favorite_languages.keys()):
+    print(f"{name.title()}, please take our poll.")
